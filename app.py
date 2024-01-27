f74ce974c0571fff7a9849c059ba63a6f204c172
Clement1991 <siawclementgyasi@gmail.com>
2024-01-27 14:29:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import apology, login_required, lookup, usd
 
-import datetime
 
 # Configure application
 app = Flask(__name__)
